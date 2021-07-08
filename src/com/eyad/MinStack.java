2f9fd9686cd5195eff0babd166c3017cc708ae87
EyadZaid <eyad.zaid@hotmail.com>
2021-07-08 16:59:35 +0300

    MinStack: pop method implementation

--- src/com/eyad/MinStack.java
+++ src/com/eyad/MinStack.java
@@ -22,14 +22,28 @@ public class MinStack<T extends Comparable<T>> {
     }
 
     public void push(T item) {
+        if (isFull()) {
+            System.out.println("Exception: The stack is full.");
+            return;
+        }
         stack.push(item);
-
         if (minStack.isEmpty() || minStack.peek().compareTo(item) > 0) {
             minStack.push(item);
         } else {
             minStack.push(minStack.peek());
         }
     }
+
+    public T pop() {
+        if (isEmpty()) {
+            System.out.println("Exception: The stack is empty.");
+            return null;
+        }
+        minStack.pop();
+        return stack.pop();
+    }
+
+
 
 
 
