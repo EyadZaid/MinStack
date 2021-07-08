53697971e59846d43f1f4bc1d255c86a9fbb8d28
EyadZaid <eyad.zaid@hotmail.com>
2021-07-08 16:30:47 +0300

    MinStack: operations stack tested

--- src/com/eyad/test/StackTest.java
+++ src/com/eyad/test/StackTest.java
@@ -19,4 +19,44 @@ public class StackTest {
         }
     }
 
+    @Test
+    public void empty_stack_test() {
+        Stack<Integer> stack = new Stack<>(10);
+        assertNull(stack.pop());
+    }
+
+    @Test
+    public void capacity_stack_test() {
+        Stack<Integer> stack = new Stack<>(0);
+        assertNull(stack.pop());
+    }
+
+    @Test
+    public void full_stack_test() {
+        Stack<Integer> stack = new Stack<>(10);
+        for (int i=1; i<=11; i++) {
+            stack.push(i);
+        }
+        assertEquals(10, stack.size());
+    }
+
+    @Test
+    public void pushAndPop_stack_test() {
+        Stack<Integer> stack = new Stack<>(10_000);
+        for (int i=1; i<=5000; i++) {
+            stack.push(i);
+        }
+
+        for (int i=5000; i>=4000; i--) {
+            assertEquals(i, stack.pop());
+        }
+
+        for (int i=4000; i<=10_000; i++) {
+            stack.push(i);
+        }
+
+        for (int i=10_000; i>0; i--) {
+            assertEquals(i, stack.pop());
+        }
+    }
 }
