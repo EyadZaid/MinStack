4b3a8f62610548fcbd35a38f2e73a10bec69bb6c
EyadZaid <eyad.zaid@hotmail.com>
2021-07-08 15:39:43 +0300

    MinStack: implement isEmpty method

--- src/com/eyad/Stack.java
+++ src/com/eyad/Stack.java
@@ -1,4 +1,19 @@
 package com.eyad;
 
-public class Stack {
+public class Stack<T> {
+    private T[] data;
+    private int capacity;
+    private int topIndex;
+
+    public Stack(int capacity) {
+        data = (T[]) new Object[capacity];
+        this.capacity = capacity;
+        topIndex = -1;
+    }
+
+    public boolean isEmpty() {
+        return topIndex == -1;
+    }
+
+
 }
