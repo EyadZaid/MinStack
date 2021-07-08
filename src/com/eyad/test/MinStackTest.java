df325250828ef5b25073cc83d3c86871f1bd5cc5
EyadZaid <eyad.zaid@hotmail.com>
2021-07-08 17:19:12 +0300

    MinStack: duplicatedMin test added

--- src/com/eyad/test/MinStackTest.java
+++ src/com/eyad/test/MinStackTest.java
@@ -9,7 +9,7 @@ import static org.junit.jupiter.api.Assertions.*;
 public class MinStackTest {
 
     @Test
-    public void simple_stack_test() {
+    public void simple_minStack_test() {
         MinStack<Integer> stack = new MinStack<>(10);
         stack.push(13);
         stack.push(6);
@@ -21,5 +21,25 @@ public class MinStackTest {
         stack.push(0);
 
         assertEquals(-5, stack.getMinimum());
+    }
+
+    @Test
+    public void duplicatedMin_minStack_test() {
+        MinStack<Integer> stack = new MinStack<>(1000);
+        var expectedMin = -982;
+
+        stack.push(expectedMin);
+
+        for (int i=1; i<=900; i++) {
+            stack.push(i);
+        }
+
+        stack.push(expectedMin);
+
+        for (int i=-400; i<=-350; i++) {
+            stack.push(i);
+        }
+
+        assertEquals(expectedMin, stack.getMinimum());
     }
 }
